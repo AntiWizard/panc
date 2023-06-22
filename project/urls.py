654f7e9c17d85cb59a555638b1470f4f352dc763
aliminuei <aliminuei@gmail.com>
2023-06-23 00:36:21 +0330

    fixed swagger

--- project/urls.py
+++ project/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('user.urls')),
     path('lottery/', include('lottery.urls')),
     path('wallet/', include('wallet.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
