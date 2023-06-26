13794212561d4430f95c7be2d98dbb022af60375
nazanin <n.vaqefi98@gmail.com>
2023-06-26 23:40:56 +0330

    Edit urls in wallet/urls.py

--- project/wallet/urls.py
+++ project/wallet/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
 
 from wallet.views import (
-    SwapView,
     CashoutListView,
     CashoutDetailView,
     TransactionLogListView,
     ConvertToUSDView,
     SwapDefaultView,
-    TransactionView,
+    FirstStepSwapView,
+    SecondStepSwapView
 )
 
 urlpatterns = [
-    path('swap/', SwapView.as_view(), name='swap'),
     path('swap/default/', SwapDefaultView.as_view(), name='swap_default'),
     path('convert-to-usd/', ConvertToUSDView.as_view(), name='convert_to_usd'),
     path('cashout-list/', CashoutListView.as_view(), name='cashout_list'),
     path('cashout/<int:pk>/', CashoutDetailView.as_view(), name='cashout_detail'),
     path('transaction-list/', TransactionLogListView.as_view(), name='transaction_list'),
-    path('transaction/', TransactionView.as_view(), name='transaction_create'),
+    path('show_swap_currencies/', FirstStepSwapView.as_view(), name='show_swap_currencies'),
+    path('Create_transaction/', SecondStepSwapView.as_view(), name='Create_transaction'),
 ]
--- project/wallet/views.py
+++ project/wallet/views.py
@@ -276,29 +276,3 @@ class TransactionLogListView(GenericAPIView):
 
         return Response(data={'message': 'OK', 'data': data}, status=200)
 
-# class TransactionView(GenericAPIView):
-#     permission_classes = [IsAuthenticatedPenc]
-#     serializer_class = SwapSerializer
-#
-#     def post(self, request):
-#         coin_from = request.data.get("coin_from", "")
-#         coin_to = request.data.get("coin_to", "")
-#         # check coins price
-#         key_1 = "https://api.binance.com/api/v3/ticker/price?symbol=" + coin_from
-#         key_2 = "https://api.binance.com/api/v3/ticker/price?symbol=" + coin_to
-#
-#         coin_from_price = (requests.get(key_1)).json()
-#         coin_to_price = (requests.get(key_2)).json()
-#
-#         coin_from_count = int(request.data.get("coin_from_count", ""))
-#         coin_to_count = int(request.data.get("coin_to_count", ""))
-#
-#         result_coin_from_price = coin_from_count * coin_from_price
-#         result_coin_to_price = coin_to_count * coin_to_price
-#
-#         if result_coin_to_price > result_coin_from_price:
-#             # check user wallet balance
-#             user_wallet_balance = Web3.eth.get_balance(request.user.wallet_address)
-#             if (user_wallet_balance + result_coin_from_price) > result_coin_to_price:
-#                 raise Exception("YOUR BALANCE IS NOT ENOUGH")
-#         # TODO CHECK coin_to_count
