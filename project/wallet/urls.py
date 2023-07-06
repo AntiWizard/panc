05736ed7405cc1e95ee0a133d79bfcbac0b5e718
nazanin <n.vaqefi98@gmail.com>
2023-07-06 18:32:29 +0330

    Comment url

--- project/wallet/urls.py
+++ project/wallet/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from wallet.views import CashoutListView, CashoutDetailView, TransactionLogListView, ConvertToUSDView, SwapDefaultView, \
-    FirstStepSwapView, SecondStepSwapView, CurrencyTypeListView
+    FirstStepSwapView, CurrencyTypeListView
 
 urlpatterns = [
     path('swap/default/', SwapDefaultView.as_view(), name='swap_default'),
@@ -11,5 +11,4 @@ urlpatterns = [
     path('cashout/<int:pk>/', CashoutDetailView.as_view(), name='cashout_detail'),
     path('transaction-list/', TransactionLogListView.as_view(), name='transaction_list'),
     path('start_swap/', FirstStepSwapView.as_view(), name='show_swap_currencies'),
-    path('Create_transaction/', SecondStepSwapView.as_view(), name='Create_transaction'),
 ]
